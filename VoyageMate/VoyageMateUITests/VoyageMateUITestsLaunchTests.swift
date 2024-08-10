e5fb08ee019768fef061f504261de6661e283143
Rodrigo Bruner <rodrigo@bruner.net.br>
2024-08-10 04:01:55 -0500

    chore:start project

---
+++ VoyageMate/VoyageMateUITests/VoyageMateUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  VoyageMateUITestsLaunchTests.swift
+//  VoyageMateUITests
+//
+//  Created by user228347 on 8/10/24.
+//
+
+import XCTest
+
+final class VoyageMateUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
